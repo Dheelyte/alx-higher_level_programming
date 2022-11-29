2d9163418441b7fa4534192e146e6c60d8e69be1
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-11-29 01:58:31 -0800

    Task 4 to 9

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/pythom3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[6] + str[-22:-18] + str[6] + str[0:6]
+print(str)
