8f38e128ea446aca878426aa58f4c18633d3e831
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-12-01 01:47:29 -0800

    Fix Task 1,2,3

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,5 +5,8 @@ if ((number % 10) > 5):
     print("Last digit of", number, "is", number % 10, "and is greater than 5")
 elif ((number % 10) == 0):
     print("Last digit of", number, "is", number % 10, "and is 0")
-elif ((number % 10) < 6 and (number % 10) != 0):
-    print("Last digit of", number, "is", number % 10, "and is less than 6 and is not 0")
+elif (abs(number % 10) < 6 and abs(number % 10) != 0):
+    if ((number % 10) < 0):
+        print(f"Last digit of {number} is -{number % 10} and is less than 6 and is not 0")
+    else:
+        print("Last digit of", number, "is", number % 10, "and is less than 6 and is not 0")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 i = chr
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) != 'q' and chr(i) != 'e':
-        print(chr(i), end="")
+        print("".format(chr(i)), end="")
