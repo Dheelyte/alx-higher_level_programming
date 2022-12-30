9f7a08a3e009842532f56057cd9c5cac934082eb
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-12-30 15:55:54 -0800

    Task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 
 for i in range(122, 96, -1):
     if (i % 2) == 0:
-        print(chr(i), end="")
+        print("{}".format(chr(i)), end="")
     else:
-        print(chr(i - 32), end="")
+        print("{}".format(chr(i - 32)), end="")
