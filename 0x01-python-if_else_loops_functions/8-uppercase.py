0be91fba84246d97ef2248e1e67b09baccc13a82
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-12-30 14:57:12 -0800

    Task 8 - Fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if ord(char) >= 65:
-            print("{}".format(chr(ord(char) - 32)), end="")
+        if ord(char) >= 97 and ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
     print("")
+uppercase("holberton :))")
