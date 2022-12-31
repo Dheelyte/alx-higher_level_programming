bec7c4439c6eb7cea38e4ec6f583b59e569397a4
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-12-31 10:30:20 -0800

    Task 2 - Fix

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def arguments():
+    from sys import argv
+    args = len(argv)
+    if args == 1:
+        print("0 arguments.")
+    elif args == 2:
+        print("1 arguement.")
+        print("{}: {}".format(1, argv[1]))
+    else:
+        print("{} arguments:".format(args - 1))
+        for i in range(1, args):
+            print("{}: {}".format(i, argv[i]))
+
+if __name__ == "__main__":
+    arguments()
