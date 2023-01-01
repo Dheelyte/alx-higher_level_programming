ab8d7337fe00d6370f904d18b4f427b27a56a9a8
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-01 01:57:54 -0800

    Task 3 - Fx

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for item in my_list:
-        print("{}".format(item))
+def print_reversed_list_integer(my_list=[]): 
+    if my_list:
+       my_list.reverse()
+       for item in my_list:
+            print("{}".format(item))
