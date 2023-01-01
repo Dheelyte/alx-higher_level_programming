f9d39e0abad647cb3ffc2f6cf40bb93ab1c76ef0
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-01 09:51:00 -0800

    Task 7 - Fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        sum_2 = 0 + tuple_b[1]
-    if len(tuple_b) < 2:
-        sum_2 = tuple_a[1] + 0
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2: 
-        sum_2 = tuple_a[1] + tuple_b[1]
-    sum_1 = tuple_a[0] + tuple_b[0]
-    return (sum_1, sum_2)
+    tuple_1 = tuple_a + (0,)
+    tuple_2 = tuple_b + (0,)
+    return (tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1])
