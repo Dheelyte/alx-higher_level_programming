ac21d3d6614096566443dad03b51cfc6f861602f
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-01 14:12:09 -0800

    Task 5

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_list = []
-    [new_list.append(item) for item in set_1 if item in set_2]
-    return new_list
+    return (set_1 & set_2)
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_list = []
-    [new_list.append(item) for item in set_1 if item not in set_2]
-    [new_list.append(item) for item in set_2 if item not in set_1]
-    return new_list
+    return (set_1 ^ set_2)
