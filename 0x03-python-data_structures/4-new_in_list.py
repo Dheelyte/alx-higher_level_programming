c4ba64d098f9a4de71ff99e4e97422142d742f58
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-01 02:25:08 -0800

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    new_list = []
+    for item in my_list:
+        new_list.append(item)
+    new_list[idx] = element
+    return new_list
