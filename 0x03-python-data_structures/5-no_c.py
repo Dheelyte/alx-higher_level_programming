ecb88e3c58cdf59031ff096c9dd8a08ec1f1134e
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-01 02:50:52 -0800

    Task 5 - Fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new_string = ""
     if my_string:
         for char in my_string:
-            if char != "c":
+            if char != "c" and char != "C":
                 new_string += char
     return new_string
