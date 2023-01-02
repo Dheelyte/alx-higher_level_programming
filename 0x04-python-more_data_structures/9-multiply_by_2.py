f5c4eb93dbf40b33b5053255f0e5cbeff9a64bdd
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-02 09:22:48 -0800

    Task 9 - Optimize

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for key, value in a_dictionary.items():
-        new_dict.update({key: value * 2})
-    return new_dict
+    return {key: value * 2 for key, value in a_dictionary.items()}
