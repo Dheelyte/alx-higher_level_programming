eb97cb961714dc1d9ac99243ed3024ccd21ab4ac
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-03 10:31:07 -0800

    Task 101 - Fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda item: list(map(lambda new_item: new_item**2, item)), matrix))
+    return list(map(lambda item: list(map(lambda x: x**2, item)), matrix))
