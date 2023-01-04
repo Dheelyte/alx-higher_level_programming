801d72587babc35fa8a147526084bca739c0d09c
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-04 00:33:26 -0800

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     length = 0
     for i in my_list[:x]:
@@ -8,4 +8,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print()
-    return length 
+    return length
