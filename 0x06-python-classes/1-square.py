dc9b2fab35bb01561b022d8b3ecd567ac981bc2c
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-08 17:15:47 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by."""
+
+
+class Square():
+
+    """A class Square that defines a square."""
+
+    def __init__(self, __size):
+        self.__size = __size
