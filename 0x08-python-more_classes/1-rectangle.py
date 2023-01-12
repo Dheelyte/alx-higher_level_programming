0c70476e1886eddd811e6fc9dd95c3d64f84b9b8
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-12 10:24:57 -0800

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module contains a class Rectangle that defines a rectangle"""
+
+
+class Rectangle():
+    """This class defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """property def width(self): to retrieve it"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """property setter def width(self, value): to set it"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """property def height(self): to retrieve it"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """property setter def height(self, value): to set it"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
