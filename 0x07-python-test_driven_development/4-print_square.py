a4fed74635cbdad2b74d74bc6eb4b5ff71f7aefb
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-11 16:19:21 -0800

    Task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module prints a square with the character #"""
+
+
+def print_square(size):
+    """a function that prints a square with the character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for col in range(size):
+            for row in range(size):
+                print("#", end="")
+            print("")
