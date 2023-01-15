8ba93724adebda6a1ea982e278cdf611b83ceb67
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-15 11:47:03 -0800

    Task 4 - Fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
         Otherwise - False.
 
     """
-    return (issubclass(type(obj), a_class) or type(obj) != a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
