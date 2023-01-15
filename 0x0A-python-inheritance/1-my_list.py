ca63731a33670f4f7fbc233ef0f7fa5bc4b81f05
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-15 04:03:43 -0800

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains class MyList that inherits from list"""
+
+class MyList(list):
+    """A subclass of list"""
+
+    def __init__(self):
+        """Initialises the list subclass"""
+        super().__init__()
+
+    def print_sorted(self):
+        """Printes sorted list"""
+        print(sorted(self))
