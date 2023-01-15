db6b01d173828b77e7baf9227f5bfc89d7a6ca34
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-15 11:33:04 -0800

    Task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,4 @@
 
 def is_kind_of_class(obj, a_class):
     """Is instance of or subclass of"""
-    return (isinstance(obj, a_class) or issubclass(obj, a_class))
+    return isinstance(obj, a_class)
