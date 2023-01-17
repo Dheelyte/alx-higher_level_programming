8916775f6a08c29a8d664944bef5e66bd00f617e
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-17 00:56:49 -0800

    Task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""""
+
+
+def add_attribute(obj, attr, value):
+    """add a new attribute to class
+
+    Args:
+        obj: object to add attribute to.
+        attr: Attribute to be added.
+        value: value of attribute to be added.
+
+    Returns:
+        TypeError if object already has an attribute
+    """
+
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
