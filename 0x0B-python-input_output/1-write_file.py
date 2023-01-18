99aa780960e0bf46b37c34e0766267584d2d9ecb
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-18 01:51:30 -0800

    Task 1 - Fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """"writes a string to a text file (UTF8)"""
 
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
