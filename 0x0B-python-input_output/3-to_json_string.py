5aaba6ee9c4735ee4222d71af5796d324f69e376
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-18 03:15:20 -0800

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains ```to_json_string``` function"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
