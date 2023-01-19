7ceb0805a3f8a5408963ce621fa119b6019c174b
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-01-19 04:23:58 -0800

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a class Student that defines a student"""
+
+
+class Student():
+    """class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization method"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+
+        if type(attrs) == list and all(type(e) == str for e in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
