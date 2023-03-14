ffa7af8ccc2cc870583377a828b7feb5efaa7861
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-03-14 15:47:57 -0700

    Fix 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const array = require('./100-data').list;
-const mappedArray = array.map(x => x * array.indexOf(x));
+const mappedArray = array.map((x, i) => x * i);
 console.log(array);
 console.log(mappedArray);
