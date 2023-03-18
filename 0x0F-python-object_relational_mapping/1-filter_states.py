d4ad5174c9cf1c7f654dc1e4a5eaa47bf74a4193
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-03-18 09:47:31 -0700

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM states WHERE name RLIKE '^[N]'")
+
+query_rows = cur.fetchall()
+
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
