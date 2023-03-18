14ede88454eee9d7563ec3761095273ecd40f6d9
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-03-18 14:12:25 -0700

    Fix Task 0 and 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """Connect to the database."""
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
     cur = conn.cursor()
