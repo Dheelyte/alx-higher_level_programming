4d65a760f95e798f7e9166d6a5055f5bce2d8963
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-03-19 12:07:21 -0700

    Redo Tasks. Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    """Connect to the database."""
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
 
     query_rows = cur.fetchall()
 
