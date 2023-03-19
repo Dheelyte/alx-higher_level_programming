7ee8035687da4b998e6f4aafdf0d53e5d81ddf47
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-03-19 12:10:39 -0700

    Fix Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+    """list all states"""
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
     cur = conn.cursor()
